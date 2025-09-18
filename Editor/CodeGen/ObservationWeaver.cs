b78c090f4dea0644e83ff809bc69b12f05c4a780
Yunshu <1315645341@qq.com>
2025-09-18 21:21:00 +0800

    在非Editor环境下无法使用 AssetDatabase

--- Editor/CodeGen/ObservationWeaver.cs
+++ Editor/CodeGen/ObservationWeaver.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using LJVoyage.ObservationToolkit.Editor.CodeGen;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Mono.Cecil.Rocks;
 using Unity.CompilationPipeline.Common.ILPostProcessing;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
 
 namespace LJVoyage.ObservationToolkit.Editor
 {
@@ -17,7 +21,29 @@ namespace LJVoyage.ObservationToolkit.Editor
 
         public override bool WillProcess(ICompiledAssembly compiledAssembly)
         {
-            return compiledAssembly.Name == "Assembly-CSharp";
+            List<string> assemblies = new List<string> { "Assembly-CSharp" };
+
+            // 非Editor 环境下  无法使用 AssetDatabase
+
+            // var guid = AssetDatabase.FindAssets("t:AssemblyGroup");
+            //
+            // if (guid != null && guid.Length > 0)
+            // {
+            //     var path = AssetDatabase.GUIDToAssetPath(guid[0]);
+            //     var asset = AssetDatabase.LoadAssetAtPath<AssemblyGroup>(path);
+            //     if (asset != null)
+            //     {
+            //         assemblies.AddRange(asset.Assemblies.Select(x => x.name));
+            //     }
+            // }
+            //
+            // foreach (var assembly in assemblies)
+            // {
+            //    Console.WriteLine(assembly);
+            // }
+
+
+            return assemblies.Any(x => x == compiledAssembly.Name);
         }
 
         public override ILPostProcessResult Process(ICompiledAssembly compiledAssembly)
@@ -40,12 +66,11 @@ namespace LJVoyage.ObservationToolkit.Editor
                     if (backingField == null) continue;
 
                     var setMethod = prop.SetMethod;
-                    
+
                     setMethod.Body.SimplifyMacros();
-                    
+
                     var il = setMethod.Body.GetILProcessor();
 
-                   
 
                     // 找到所有 stfld 指令对应 backingField
                     var instructions = setMethod.Body.Instructions.ToList();
@@ -57,7 +82,7 @@ namespace LJVoyage.ObservationToolkit.Editor
                     if (stfldIndices.Count == 0) continue;
 
                     VariableDefinition lastTmp = new VariableDefinition(backingField.FieldType);
-                    
+
                     // 将每个 stfld 替换为 stloc tmpVar
                     foreach (var idx in stfldIndices)
                     {
@@ -65,7 +90,7 @@ namespace LJVoyage.ObservationToolkit.Editor
                         lastTmp = new VariableDefinition(backingField.FieldType);
                         setMethod.Body.Variables.Add(lastTmp);
                         setMethod.Body.InitLocals = true;
-                        
+
                         var storeTmp = il.Create(OpCodes.Stloc, lastTmp);
                         il.Replace(instructions[idx], storeTmp);
                     }
@@ -75,27 +100,25 @@ namespace LJVoyage.ObservationToolkit.Editor
                     var genericSetField = new GenericInstanceMethod(setFieldRef);
                     genericSetField.GenericArguments.Add(backingField.FieldType);
 
-                   
-                    
-                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldarg_0));    // this -> binding
+
+                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldarg_0)); // this -> binding
                     il.InsertBefore(lastInstr, il.Create(OpCodes.Ldflda, backingField)); // ref _field
-                    
-                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldloc, lastTmp));        // 临时变量
-                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldstr, prop.Name));     // 属性名
+
+                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldloc, lastTmp)); // 临时变量
+                    il.InsertBefore(lastInstr, il.Create(OpCodes.Ldstr, prop.Name)); // 属性名
                     il.InsertBefore(lastInstr, il.Create(OpCodes.Call, genericSetField));
-                    il.InsertBefore(lastInstr, il.Create(OpCodes.Pop));                   // 丢弃 bool
-                    
-                    
+                    il.InsertBefore(lastInstr, il.Create(OpCodes.Pop)); // 丢弃 bool
+
+
                     setMethod.Body.Optimize();
-                    
-                    
                 }
             }
 
             using (var ms = new MemoryStream())
             {
                 module.Write(ms);
-                return new ILPostProcessResult(new InMemoryAssembly(ms.ToArray(), compiledAssembly.InMemoryAssembly.PdbData));
+                return new ILPostProcessResult(new InMemoryAssembly(ms.ToArray(),
+                    compiledAssembly.InMemoryAssembly.PdbData));
             }
         }
 
@@ -170,6 +193,7 @@ namespace LJVoyage.ObservationToolkit.Editor
                 if (types[i].HasNestedTypes)
                     types.AddRange(types[i].NestedTypes);
             }
+
             return types;
         }
     }
